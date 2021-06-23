e900927cb829e38b2ecc53ef19aa176edd970a96
ametlik <MonProjet2020>
2021-06-23 10:24:31 +0200

    animals data

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -34,8 +34,15 @@ class ProductFixtures extends Fixture
     {
         return [
             [
-                'title' => 'laisse pour grand chien',
-                'description' => 'laisse adaptée pour les grands chien, dérouleur résistant avec blocage',
+                'title' => 'Laisse pour grand chien',
+                'description' => 'Laisse adaptée pour les grands chien, dérouleur résistant avec blocage',
+                'category' => 'accessories',
+                'price' => 29.99,
+                'path' => 'img/products/accessories/laisse_001.jpeg'
+            ],
+            [
+                'title' => 'Laisse pour grand chien',
+                'description' => 'Laisse adaptée pour les grands chien, dérouleur résistant avec blocage',
                 'category' => 'accessories',
                 'price' => 29.99,
                 'path' => 'img/products/accessories/laisse_001.jpeg'
@@ -43,15 +50,51 @@ class ProductFixtures extends Fixture
 
 
             [
-                'title' => 'os à ronger',
-                'description' => 'os pour petit chien, renforcant la machoire et assimilé comme une récompense pour le chien',
+                'title' => 'Os à ronger',
+                'description' => 'Os pour petit chien, renforcant la machoire et assimilé comme une récompense pour le chien.',
                 'category' => 'food',
                 'price' => 3.99,
                 'path' => 'img/products/food/os_001.jpeg'
             ],
+            [
+                'title' => 'Royal Canin Medium Adult pour chien',
+                'description' => 'Croquettes pour les chiens adultes de races moyennes (11 - 25 kg) de 12 mois à 7 ans, renforcent les défenses immunitaires naturelles et le système digestif, avec un complexe d\'antioxydants.',
+                'category' => 'food',
+                'price' => 18.99,
+                'path' => 'img/products/food/royal-canin-medium.jpg'
+            ],
+            [
+                'title' => 'Royal Canin Sterilised en sauce pour chat',
+                'description' => 'Sachets fraîcheur de nourriture humide pour chat adulte stérilisé dès 1 an, délicieuses bouchées, favorisent un système urinaire sain, contribuent à maintenir le poids de forme, particulièrement savoureux.',
+                'category' => 'food',
+                'price' => 12.49,
+                'path' => 'img/products/food/royal-canin-sterilised.jpg'
+            ],
+            [
+                'title' => 'Hill\'s Prescription Diet Metabolic Weight Management poulet pour chat',
+                'description' => 'Croquettes diététiques pauvres en calories pour les chats en surpoids, favorisent la perte de poids ou le maintien du poids après un régime, teneur en énergie réduite, 23 % de poulet.',
+                'category' => 'food',
+                'price' => 17.49,
+                'path' => 'img/products/food/hills-prescription-diet.jpg'
+            ],
+            [
+                'title' => 'Purizon Adult Sterilised poulet, poisson - sans céréales pour chat',
+                'description' => 'Croquettes pour les chats stérilisés dont le métabolisme change et les besoins énergétiques se réduisent, pauvres en graisses, riches en protéines (poulet), sans céréales.',
+                'category' => 'food',
+                'price' => 4.99,
+                'path' => 'img/products/food/purizon-adult.jpg'
+            ],
+            [
+                'title' => 'PURINA PRO PLAN All sizes Adult Light/Sterilised poulet',
+                'description' => 'Croquettes pauvres en graisses pour chien adulte de toutes les tailles stérilisé ou en surpoids. Formule OPTIWEIGHT pour une perte de poids progressive et durable. Régulent la sensation de faim.',
+                'category' => 'food',
+                'price' => 34.99,
+                'path' => 'img/products/food/purina-pro.jpg'
+            ],
 
 
-            Royal Canin Medium Adult pour chien
+            
+            
 
             [
                 'title' => 'antiparasite',
