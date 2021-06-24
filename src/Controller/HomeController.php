38014d3edd6e3fddff2514fbc7bdc0f5fa7ba206
Guigs95 <dominici.guillaume@gmail.com>
2021-06-24 15:32:28 +0200

    Front Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,16 +5,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use App\Entity\Blog;
+use App\Entity\Animal;
+use App\Entity\Type;
+
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/", name="home")
-     */
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
+	/**
+	 * @Route("/", name="home")
+	 */
+	public function index(): Response
+	{
+		$products = $this->getDoctrine()->getRepository(Product::class)->findBy([], [],5);
+		$blogs = $this->getDoctrine()->getRepository(Blog::class)->findBy([],['datecreated' => 'DESC'], 5);
+		$animals = $this->getDoctrine()->getRepository(Animal::class)->findBy([], [],5);
+
+		// totaux
+		$total_products = $this->getDoctrine()->getRepository(Product::class)->findAll();
+		$total_blogs = $this->getDoctrine()->getRepository(Blog::class)->findAll();
+		$total_animals = $this->getDoctrine()->getRepository(Animal::class)->findAll();
+
+		return $this->render('home/index.html.twig', [
+			'products'   => $products,
+			'total_products' => count($total_products),
+			'blogs'      => $blogs,
+			'total_blogs' => count($total_blogs),
+			'animals'    => $animals,
+			'total_animals' => count($total_animals),
+		]);
+	}
 }
