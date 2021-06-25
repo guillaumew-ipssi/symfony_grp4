5ad8f9184e9417f5fde12f5f16d069dbb859457d
Guillaume <guillaume.waroux@gmail.com>
2021-06-25 15:06:21 +0200

    change word to search form

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -20,6 +20,7 @@ class SearchType extends AbstractType
                 ]
             ])
             ->add('submit', SubmitType::class, [
+                'label' => 'Rechercher',
                 'attr' => [
                     'class' => 'btn btn-dark'
                 ]
